f176a08b8e1be2f52f3cd8acbd278ee9c19a2ef2
David Kane <d.r.kane@gmail.com>
2019-02-08 23:03:27 +0000

    remove un-needed scripts

--- app.py
+++ app.py
@@ -8,17 +8,6 @@ app = dash.Dash(
         {"http-equiv": "X-UA-Compatible", "content": "IE=edge,chrome=1"},
         {"name": "viewport", "content": "width=device-width, initial-scale=1.0"},
     ],
-    external_scripts=[
-        {
-            'src': "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js",
-            'integrity': "sha256-ImQvICV38LovIsvla2zykaCTdEh1Z801Y+DSop91wMU=",
-            'crossorigin': "anonymous"
-        }, {
-            'src': "https://cdnjs.cloudflare.com/ajax/libs/is-in-viewport/3.0.4/isInViewport.min.js",
-            'integrity': "sha256-YCKf7pbD5WuWira7Ir49rglmeklV67h8HCeC7GCYWEw=",
-            'crossorigin': "anonymous"
-        }
-    ],
     external_stylesheets=[
         "https://use.typekit.net/nri0jbb.css",
         "https://fonts.googleapis.com/icon?family=Material+Icons"
