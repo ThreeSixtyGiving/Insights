3112ee4c6abd9eb9ef97f51f9b3ee58a57aceb47
David Kane <d.r.kane@gmail.com>
2019-09-06 00:13:36 +0100

    add a test javacript page

--- tsg_insights/blueprints/api.py
+++ tsg_insights/blueprints/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, Response, abort
+from flask import Blueprint, render_template, jsonify, request, Response, abort
 from sqlalchemy import func
 
 from ..data.models import Grant
@@ -22,6 +22,11 @@ def api_endpoint():
     return {
         "by_funder": [r._asdict() for r in result]
     }
+
+
+@bp.route('/test/<dataset>')
+def test_dash(dataset):
+    return render_template('test_dash.html.j2', dataset=dataset)
 
 
 bp.add_url_rule(
