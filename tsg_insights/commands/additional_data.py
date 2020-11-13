febfccc22e8896ea20d53d9ce4d4b370082e66a2
David Kane <david@dkane.net>
2020-11-13 09:25:59 +0000

    add option for resetting the cache

---
+++ tsg_insights/commands/additional_data.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+import click
+from flask import Flask, current_app
+from flask.cli import AppGroup, with_appcontext
+import pandas as pd
+
+from ..data.registry import process_registry, get_reg_file
+from ..data.process import get_dataframe_from_url
+from ..data.cache import delete_from_cache, get_from_cache, get_cache, save_to_cache
+
+cli = AppGroup('additional_data')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ])
+
+
+@cli.command('removeall')
+@with_appcontext
+def cli_remove_all_files():
+    cache = get_cache()
+    for k in ["charity", "company", "postcode"]:
+        keys_to_delete = cache.hlen(k)
+        if click.confirm(f'This will delete all {keys_to_delete:,.0f} cached {k}. Are you sure you want to continue?'):
+            cache.delete(k)
+            click.echo(f"Deleted {keys_to_delete:,.0f} keys from {k}")
