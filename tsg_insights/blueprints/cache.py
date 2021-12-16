efe66ed2f48f95450b6e2bfa7977108e839d11c1
Michael Wood <michael.wood@opendataservices.coop>
2021-12-16 15:32:33 +0000

    tsg_insights: Add cache reloader form
    This is a quick hack to allow manually refreshing of the cache. From the
    front end web.

--- tsg_insights/blueprints/cache.py
+++ tsg_insights/blueprints/cache.py
@@ -1,9 +1,10 @@
 import json
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, render_template, request
 
-from ..data.cache import get_cache
+from ..data.cache import delete_from_cache, get_cache
 from ..data.process import fetch_geocodes
+from .fetch import get_registry_file
 
 bp = Blueprint("cache", __name__)
 
@@ -19,6 +20,21 @@ def check_redis_cache():
         },
     }
     return json.dumps(cache_contents)
+
+
+@bp.route("/reload/", methods=("GET", "POST"))
+def reload_file():
+    context = {}
+
+    if request.method == "POST":
+        file_id = request.form["fileid"]
+        delete_from_cache(file_id)
+        ret = get_registry_file(file_id)
+
+        context["job"] = ret.data.decode("utf-8")
+        context["file_id"] = file_id
+
+    return render_template("cache_reloader.html.j2", **context)
 
 
 @bp.route("/geocodes")
